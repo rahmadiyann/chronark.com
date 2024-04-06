84e02c9a1a0a221f07ffe31437e19a89e92cf569
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2024-04-06 11:07:59 +0700

    .

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://linkedin.com/in/rahmadiyan",
+		label: "LinkedIn",
+		handle: "@rahmadiyan",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:muhammad@rahmadiyan.id",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "muhammad@rahmadiyan.id",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/rahmadiyann",
 		label: "Github",
-		handle: "chronark",
+		handle: "rahmadiyann",
 	},
 ];
 
